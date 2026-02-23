dbf08526b6413c962aef03b998f117f13968a425
lazymisu <felix.chacaltana.felix@gmail.com>
2026-02-23 00:16:49 -0500

    Add dependency manager

---
+++ IMDUMB/App/DI/DependencyManager.swift
@@ -0,0 +1,14 @@
+//
+//  DependencyManager.swift
+//  IMDUMB
+//
+//  Created by felix on 22/02/26.
+//
+
+final class DependencyManager {
+    static let shared = DependencyManager()
+    
+    private init() {}
+    
+    // TODO: Add dependencies...
+}
