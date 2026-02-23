8db94db65867fd93b74930e889dcb905ea40bc9e
lazymisu <felix.chacaltana.felix@gmail.com>
2026-02-23 01:31:03 -0500

    Update splash view controller

--- IMDUMB/Presentation/Splash/View/SplashViewController.swift
+++ IMDUMB/Presentation/Splash/View/SplashViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SplashViewController: UIViewController {
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var lblWelcome: UILabel!
     
     // MARK: - Properties
     
@@ -18,7 +19,7 @@ class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter = SplashPresenter(view: self)
+        presenter = DependencyManager.shared.makeSplashPresenter(view: self)
         presenter.viewDidLoad()
     }
 }
@@ -36,10 +37,17 @@ extension SplashViewController: SplashViewProtocol {
     }
     
     func navigateToHome() {
-        // TODO: Push to home screen
+        let homeVC = HomeViewController(nibName: "HomeViewController", bundle: nil)
+        navigationController?.pushViewController(homeVC, animated: true)
     }
     
     func showError(message: String) {
-        // TODO: Show alert with message
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        present(alert, animated: true)
+    }
+    
+    func showWelcomeMessage(_ message: String) {
+        lblWelcome.text = message
+        lblWelcome.isHidden = false
     }
 }
